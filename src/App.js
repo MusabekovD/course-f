e0fe669a6d7019a0e51f659732bc33a9fce58414
MusabekovD <diyormusabekov1407@gmail.com>
2023-11-25 21:20:10 +0500

    The

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-
-
+import {BrowserRouter as Routes, Router, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Test from "./pages/Test";
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route  path="/" element={<Home />} />
+          <Route  path="/test" element={<Test />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
