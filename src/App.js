e3e314c4d064019cb484fae0e0d82e6d16c58b6f
MusabekovD <diyormusabekov1407@gmail.com>
2023-11-25 20:41:35 +0500

    installed tailwind css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "./pages/auth/Auth";
-import Expense from "./pages/expense-tracker/Expense";
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Auth />} />
-          <Route  path="/expense-tracker" element={<Expense />}/>
-        </Routes> 
-      </Router>
+     
     </div>
   );
 }
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 
 const firebaseConfig = {
@@ -15,6 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const provider = new  GoogleAuthProvider()
 
