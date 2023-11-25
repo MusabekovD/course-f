14ef39957720f1b32dcd900a6793dca52aa16d67
Musabekov <diyormusabekov1407@gmail.com>
2023-11-25 18:58:26 +0300

    first commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRAbSB56-bR2unqfrADQgGwCgnZy8pW9A",
+  authDomain: "fir-c-9db2a.firebaseapp.com",
+  projectId: "fir-c-9db2a",
+  storageBucket: "fir-c-9db2a.appspot.com",
+  messagingSenderId: "1087585220007",
+  appId: "1:1087585220007:web:92766e66c391210deeb282",
+  measurementId: "G-J1WESPR442"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const provider = new  GoogleAuthProvider()
+
---
+++ src/pages/expense-tracker/Expense.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const  Expense = () => {
+  return (
+    <div>
+        <h2>Expense Tracker</h2>
+    </div>
+  )
+}
+
+export default  Expense
